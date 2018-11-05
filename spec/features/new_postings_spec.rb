7d65c8f828ef41918994703bbd9c2e2fbb365d63
Irina Baldwin <irinabaldwin@Irinas-MacBook-Air.local>
2018-11-05 14:53:26 +0000

    New posting form, feature tested

--- spec/features/new_postings_spec.rb
+++ spec/features/new_postings_spec.rb
@@ -1,14 +1,13 @@
 feature 'new postings' do
   scenario 'user can add a new posting' do
     visit('/postings/new')
-    expect(page).to have_content("list a space")
-
-    expect(page).to have_css("input[type='text'][placeholer='Title']")
-    expect(page).to have_css("input[type='text'][placeholer='Description']")
-    expect(page).to have_content("Price")
-    expect(page).to have_content("Available From")
-    expect(page).to have_content("Available To")
-    expect(page).to have_selector(:link_or_button, 'List my space')
+    expect(page).to have_content("List a space")
+    expect(page).to have_content("Title")
+    expect(page).to have_content("Description")
+    expect(page).to have_content("Price per night (£)")
+    expect(page).to have_content("Available from (DD/MM/YY)")
+    expect(page).to have_content("Available to (DD/MM/YY)")
+    expect(page).to have_selector(:button, "Submit")
   end
 
 end
