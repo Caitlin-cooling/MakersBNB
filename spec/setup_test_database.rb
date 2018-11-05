38c01519aeb349f2282c099c5ee7850d6263c5e4
Irina Baldwin <irinabaldwin@Irinas-MacBook-Air.local>
2018-11-05 13:50:44 +0000

    Replace truncate with drop in set up test database

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,7 @@ require 'pg'
 def setup_test_database
   # p "Setting up test database..."
   connection = PG.connect(dbname: 'makersbnb_test')
-  connection.exec("TRUNCATE postings, users;")
+  connection.exec("DROP TABLE IF EXISTS postings, users CASCADE;")
+  connection.exec("CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(60), email VARCHAR(60), password VARCHAR(60));")
+  connection.exec("CREATE TABLE postings(id SERIAL PRIMARY KEY, post VARCHAR(140), date TIMESTAMP, user_id INTEGER REFERENCES users(id) ON DELETE CASCADE);")
 end
