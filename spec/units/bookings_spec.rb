f4cb3344c36579037f5284b359b7536b5efa90f2
becc-mu <becc.mulugeta@gmail.com>
2018-11-06 15:03:02 +0000

    create booking test

--- spec/units/bookings_spec.rb
+++ spec/units/bookings_spec.rb
@@ -3,7 +3,8 @@ require 'booking'
 describe Booking do
   describe '.create' do
     it 'should create a booking object' do
-      
+      booking = Booking.create(owner_id: '1', posting_id: '1', user_id: '2')
+      expect(booking).to be_a Booking
     end
   end
 end
