a2611c041a1a451a3c407324710506a4113fe93a
jebax <joel.eliot.baxter@gmail.com>
2018-11-06 14:45:15 +0000

    added bookings table to database

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,3 +1,5 @@
+require_relative '../lib/database_connection'
+
 def sign_up_as_test_user
   visit '/'
   fill_in :name,with: 'Stan Testson'
@@ -6,6 +8,17 @@ def sign_up_as_test_user
   click_button :Submit
 end
 
-def insert_user_into_test_database
+def insert_users_into_test_database
   User.create(name: 'Stan Testson', email: 'stan@stan.com', password: 'password123')
+  User.create(name: 'Alice Bobson', email: 'albobson@gmail.com', password: 'password321')
+end
+
+def insert_posting_into_test_database
+  Posting.create('My first posting', 'Description', '100' '1')
+end
+
+def insert_booking_into_test_database
+  insert_users_into_test_database
+  insert_posting_into_test_database
+  DatabaseConnection.query("INSERT INTO bookings(posting_id,owner_id,user_id) VALUES('1','1','2');")
 end
